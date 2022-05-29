97104329e41a2e1c5249183cc69774585c0c4b64
Fernando <fernando.luiz92@hotmail.com>
2022-05-29 12:30:31 -0300

    Mocks

--- src/test/java/com/app/CalculadoraTest.java
+++ src/test/java/com/app/CalculadoraTest.java
@@ -1,22 +1,46 @@
 package com.app;
 
-import static org.junit.Assert.assertEquals;
+
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertEquals;
+
 
 public class CalculadoraTest {
-
+	
+	/**
+	 * assertEquals - 
+	 *  - Afirmacao : metodo para testar o resultado esperado
+	 *  - Asserions : para tipos primitivos, Objetos e arrays
+	 *  - Import static
+	 *  - Ordem dos parametros : Valor Esperado, Valor Atual
+	 */
 	@Test
 	public void testSomar() {
 		Calculadora calc = new Calculadora();
 		int soma = calc.somar("1+1+3");
 		assertEquals(5, soma);
 	}
+	
+	/**
+	 * mock - objetos simulados, é um termo utilizado para descrever quando simulamos o comportamento de objetos reais
+	 * de forma controlada
+	 */
+	@Test
+	public void testSomarCondMock() {
+		Calculadora calculadora = mock(Calculadora.class);
+		
+		when(calculadora.somar("1+2")).thenReturn(10);
+		
+		int resultado = calculadora.somar("1+2");
+		
+		assertEquals(10, resultado);
+					
+	}
 }
-/**
- * assertEquals - 
- *  - Afirmacao : metodo para testar o resultado esperado
- *  - Asserions : para tipos primitivos, Objetos e arrays
- *  - Import static
- *  - Ordem dos parametros : Valor Esperado, Valor Atual
- */
+
